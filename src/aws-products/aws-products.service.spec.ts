55cf09fa77fcef76766596c5085b80c5dd32a290
MotaGuilherme <metatronmota@gmail.com>
2023-03-31 03:47:10 -0300

    test: unit tests

--- src/aws-products/aws-products.service.spec.ts
+++ src/aws-products/aws-products.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AwsProductsService } from './aws-products.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('AwsProductsService', () => {
   let service: AwsProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AwsProductsService],
+      providers: [
+        AwsProductsService,
+        {
+          provide: getModelToken('Products'),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<AwsProductsService>(AwsProductsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('scrape', () => {
+    it('should scrape data from all urls and save to database', async () => {
+      const consoleLogSpy = jest.spyOn(console, 'log');
+      const productsModelSpy = jest.spyOn(service, 'scrape');
+      await service.scrape();
+      expect(consoleLogSpy).toHaveBeenCalled();
+      expect(productsModelSpy).toHaveBeenCalled();
+    });
   });
+
+  describe('scrape', () => {
+    it('should scrape data from all urls and save to database', async () => {
+      const consoleLogSpy = jest.spyOn(console, 'log');
+      const productModelSaveSpy = jest.spyOn(service.productsModel.prototype, 'save');
+      await service.scrape();
+      expect(consoleLogSpy).toHaveBeenCalled();
+      expect(productModelSaveSpy).toHaveBeenCalled();
+    });
+  });
+
 });
+
