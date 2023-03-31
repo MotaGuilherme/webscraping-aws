6b595ca81d15f04f35b438fea8488f4c37388305
MotaGuilherme <metatronmota@gmail.com>
2023-03-31 03:49:00 -0300

    feat: implementing dependency injections

--- src/aws-products/aws-products.module.ts
+++ src/aws-products/aws-products.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import {MomentModule} from "@ccmos/nestjs-moment";
+
 import { AwsProductsController } from './aws-products.controller';
+import {ProductsSchema} from "./interfaces/products.schema";
+import {MongooseModule} from "@nestjs/mongoose";
+import {AwsProductsService} from "./aws-products.service";
 
 @Module({
-  controllers: [AwsProductsController]
+  imports: [MongooseModule.forFeature([{name: 'Products', schema: ProductsSchema}]),
+    MomentModule.forRoot({
+      tz: 'Brasil/Sao-Paulo',})],
+  controllers: [AwsProductsController],
+  providers: [AwsProductsService]
 })
 export class AwsProductsModule {}
